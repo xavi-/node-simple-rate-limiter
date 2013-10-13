240cdba8fee7fc16b5863a409a2f30da14022774
Xavi <xavi.rmz@gmail.com>
2013-10-13 11:49:01 -0700

    Added package file.

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+  "name": "simple-rate-limiter",
+  "version": "0.1.0",
+  "description": "A laughably simplistic router for node.js",
+  "keywords": [
+    "throttle",
+    "rate limit",
+    "queue calls"
+  ],
+  "maintainers": [
+    {
+      "name": "Xavi",
+      "email": "xavi.rmz@gmail.com",
+      "web": "http://xavi.co"
+    }
+  ],
+  "main": "./index.js",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/xavi-/node-simple-rate-limiter.git"
+  }
+}
