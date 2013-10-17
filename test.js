2b52b79d0010af4fe4187e7229708dcdc5f62f03
Xavi <xavi.rmz@gmail.com>
2013-10-17 01:03:26 -0700

    Added code to ensure all tests are ran.

--- test.js
+++ test.js
@@ -3,21 +3,27 @@ var limit = require("./");
 
 const SLOP = 2; // Timers don't seem accurate to the millisecond
 
-function runTest(count, to, per) {
-	var done = function() {
-		for(var i = 0; i < count - to; i++) {
-			var diff = times[i + to].time - times[i].time + SLOP;
-			assert.ok(per <= diff, "Diff found: " + diff + "; Expected: " + per);
-		}
-		times.forEach(function(time, idx) { assert.equal(idx, time.idx); });
+var tests = {
+	expected: 4,
+	executed: 0,
+	finished: function() { tests.executed++; }
+};
+function done(times, count, to, per) {
+	for(var i = 0; i < count - to; i++) {
+		var diff = times[i + to].time - times[i].time + SLOP;
+		assert.ok(per <= diff, "Diff found: " + diff + "; Expected: " + per);
 	}
+	times.forEach(function(time, idx) { assert.equal(idx, time.idx); });
+	tests.finished();
+}
 
+function runTest(count, to, per) {
 	var times = [];
 	var saveTime = limit(function(idx) {
 		times.push({ idx: idx, time: Date.now() });
 		console.log("executed idx: %d/%d", idx, count);
 
-		if(count <= times.length) { done(); }
+		if(count <= times.length) { done(times, count, to, per); }
 	}).to(to).per(per);
 
 	for(var i = 0; i < count; i++) {
@@ -30,4 +36,7 @@ runTest(25, 1, 100);
 runTest(2500, 100, 10);
 runTest(101, 73, 1409);
 
-console.log("\n\nDone. All test pasted...");
+process.on("exit", function() {
+	assert.equal(tests.executed, tests.expected);
+	console.log("\n\nAll done.  Everything passed.");
+});
