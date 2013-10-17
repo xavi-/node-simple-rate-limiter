c57e2952f84751176bd2f6bb6473dc89707abf33
Xavi <xavi.rmz@gmail.com>
2013-10-17 01:00:07 -0700

    Generalized test.

--- test.js
+++ test.js
@@ -1,24 +1,33 @@
 var assert = require("assert");
 var limit = require("./");
 
-const COUNT = 50, TO = 10, PER = 1000;
+const SLOP = 2; // Timers don't seem accurate to the millisecond
 
-function done() {
-	for(var i = 0; i < COUNT - TO; i++) {
-		var diff = times[i + TO].time - times[i].time;
-		assert.ok(PER <= diff, "Diff found: " + diff);
+function runTest(count, to, per) {
+	var done = function() {
+		for(var i = 0; i < count - to; i++) {
+			var diff = times[i + to].time - times[i].time + SLOP;
+			assert.ok(per <= diff, "Diff found: " + diff + "; Expected: " + per);
+		}
+		times.forEach(function(time, idx) { assert.equal(idx, time.idx); });
 	}
-	times.forEach(function(time, idx) { assert.equal(idx, time.idx); });
+
+	var times = [];
+	var saveTime = limit(function(idx) {
+		times.push({ idx: idx, time: Date.now() });
+		console.log("executed idx: %d/%d", idx, count);
+
+		if(count <= times.length) { done(); }
+	}).to(to).per(per);
+
+	for(var i = 0; i < count; i++) {
+		saveTime(i);
+	}
 }
 
-var times = [];
-var saveTime = limit(function(idx) {
-	times.push({ idx: idx, time: Date.now() });
-	console.log("executed idx: %d/%d", idx, COUNT);
+runTest(50, 10, 1000);
+runTest(25, 1, 100);
+runTest(2500, 100, 10);
+runTest(101, 73, 1409);
 
-	if(COUNT <= times.length) { done(); }
-}).to(TO).per(PER);
-
-for(var i = 0; i < COUNT; i++) {
-	saveTime(i);
-}
+console.log("\n\nDone. All test pasted...");
