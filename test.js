4910197304c04c7ec69316e707dbf89e9c48bbc0
Xavi <xavi.rmz@gmail.com>
2013-10-17 01:23:31 -0700

    Added test cases from README.

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var limit = require("./");
 const SLOP = 5; // Timers don't seem accurate to the millisecond
 
 var tests = {
-	expected: 4,
+	expected: 288,
 	executed: 0,
 	finished: function() { tests.executed++; }
 };
@@ -31,10 +31,36 @@ function runBasicTest(count, to, per) {
 	}
 }
 
+function runTickTests(count, per) {
+	var tick = limit(function() {
+		if(!tick.lastCalled) { tick.lastCalled = Date.now(); }
+		else {
+			var now = Date.now();
+			assert.ok(per <= (now - tick.lastCalled) + SLOP);
+			tick.lastCalled = now;
+		}
+		tests.finished();
+	}).per(per);
+	for(var i = 0; i < count; i++) { tick(); }
+}
+
 runBasicTest(50, 10, 1000);
 runBasicTest(25, 1, 100);
 runBasicTest(2500, 100, 10);
 runBasicTest(101, 73, 1409);
+
+runTickTests(3, 1000);
+runTickTests(25, 100);
+runTickTests(250, 10);
+runTickTests(3, 1409);
+
+var init = limit(function() { assert.ok(init.calls++ < 1); tests.finished(); });
+init.calls = 0;
+for(var i = 0; i < 5; i++) { init(); }
+
+var dos = limit(function() { assert.ok(dos.calls++ < 2); tests.finished(); }).to(2);
+dos.calls = 0;
+for(var i = 0; i < 5; i++) { dos(); }
 
 process.on("exit", function() {
 	assert.equal(tests.executed, tests.expected);
