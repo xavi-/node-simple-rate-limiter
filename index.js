bea9a0e8062d50f368b5d9aef8c717f3670cae25
Xavi <xavi.rmz@gmail.com>
2013-10-18 04:22:55 -0700

    Fixed sneaky bug caused by erratic calls to a limited method.

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 var slice = Array.prototype.slice;
 
 module.exports = function limit(fn) {
-	var _to = 1, _per = -1, available = 1;
-	var queue = [], timer;
+	var _to = 1, _per = -1;
+	var pastExecs = [], queue = [], timer;
+
+	var exec = function(fn, args) {
+		pastExecs.push(Date.now());
+		fn.apply(null, args);
+	};
 
 	var pump = function() {
-		available = _to;
+		var now = Date.now();
 
-		while(available > 0 && queue.length > 0) {
-			fn.apply(null, queue.shift());
-			available -= 1;
+		pastExecs = pastExecs.filter(function(time) { return (now - time < _per); });
+
+		while(pastExecs.length < _to && queue.length > 0) {
+			exec(fn, queue.shift());
 		}
 
-		if(available === _to) { timer = null; }
-		else { timer = setTimeout(pump, _per); }
+		if(pastExecs.length <= 0) { timer = null; }
+		else { timer = setTimeout(pump, _per - (now - pastExecs[0])); }
 	};
 
 	var limiter = function() {
-		if(available > 0) { fn.apply(null, arguments); available -= 1; }
+		if(pastExecs.length < _to) { exec(fn, arguments); }
 		else if(_per > -1) { queue.push(slice.call(arguments, 0)); }
 
 		if(!timer && _per > -1) { timer = setTimeout(pump, _per); }
 	};
 
 	limiter.to = function(count) {
-		available = (available - _to) + count;
 		_to = count;
 		return limiter;
 	};
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var limit = require("./");
 const SLOP = 5; // Timers don't seem accurate to the millisecond
 
 var tests = {
-	expected: 288,
+	expected: 289,
 	executed: 0,
 	finished: function() { tests.executed++; }
 };
@@ -43,6 +43,46 @@ function runTickTests(count, per) {
 	}).per(per);
 	for(var i = 0; i < count; i++) { tick(); }
 }
+
+
+(function runErraticQueueTest(count, to, per) {
+	var times = [];
+	var done = function() {
+		for(var i = 0; i < count - to; i++) {
+			var diff = (times[i + to].time - times[i].time) + SLOP;
+			assert.ok(per <= diff, "Diff found: " + diff + "; Expected: " + per + "; i: " + i);
+		}
+		times.forEach(function(time, idx) { assert.equal(idx, time.idx); });
+		tests.finished();
+	};
+	var saveTime = limit(function(idx) {
+		times.push({ idx: idx, time: Date.now() });
+		console.log("Erratic executed idx: %d/%d", idx, count);
+
+		if(count <= times.length) { done(times, count, to, per); }
+	}).to(to).per(per);
+
+	setTimeout(function() { saveTime(0) }, 0);
+	setTimeout(function() { saveTime(1) }, 800);
+	setTimeout(function() { saveTime(2) }, 800);
+	setTimeout(function() { saveTime(3) }, 800);
+	setTimeout(function() { saveTime(4) }, 850);
+	setTimeout(function() { saveTime(5) }, 850);
+	setTimeout(function() { saveTime(6) }, 850);
+	setTimeout(function() { saveTime(7) }, 1050);
+	setTimeout(function() { saveTime(8) }, 1100);
+	setTimeout(function() { saveTime(9) }, 1100);
+	setTimeout(function() { saveTime(10) }, 1100);
+	setTimeout(function() { saveTime(11) }, 1100);
+	setTimeout(function() { saveTime(12) }, 1800);
+	setTimeout(function() { saveTime(13) }, 1800);
+	setTimeout(function() { saveTime(14) }, 1800);
+	setTimeout(function() { saveTime(15) }, 1800);
+	setTimeout(function() { saveTime(16) }, 1800);
+	setTimeout(function() { saveTime(17) }, 2100);
+	setTimeout(function() { saveTime(18) }, 2100);
+	setTimeout(function() { saveTime(19) }, 2100);
+})(20, 10, 1000);
 
 runBasicTest(50, 10, 1000);
 runBasicTest(25, 1, 100);
